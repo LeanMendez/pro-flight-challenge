25a57a091ba01c94decff8fcef94003c843bb9a8
Leandro A. Mendez <leandroa.mendez@gmail.com>
2023-01-25 03:32:03 -0300

    fix: typo readme, color hover button

--- src/components/LogosList/LogosList.tsx
+++ src/components/LogosList/LogosList.tsx
@@ -19,22 +19,40 @@ export const LogosList: React.FC = () => {
   const [selectedIndexLogo, setSelectedIndexLogo] = useState<number>(1)
 
   const next = (): void => {
-    const nextIndex = selectedIndexLogo < logos.length ? selectedIndexLogo + 1 : 1
+    const nextIndex =
+      selectedIndexLogo < logos.length ? selectedIndexLogo + 1 : 1
     setSelectedIndexLogo(nextIndex)
   }
 
   const previous = (): void => {
-    const previousIndex = selectedIndexLogo > 1 ? selectedIndexLogo - 1 : logos.length
+    const previousIndex =
+      selectedIndexLogo > 1 ? selectedIndexLogo - 1 : logos.length
     setSelectedIndexLogo(previousIndex)
   }
 
-  return <>
-  <div className={styles.container}>
-  <button className={styles.button} onClick={previous}><img src={leftArrow}/></button>
-  {logos.map(logo => <img className={`${styles.images} ${selectedIndexLogo === logo.id ? styles.active : styles.inactive}`} key={logo.id} src={logoBrand}/>)}
-  <button className={styles.button} onClick={next}><img src={rightArrow}/></button>
-  </div>
-  </>
+  return (
+    <>
+      <div className={styles.container}>
+        <button className={styles.button} onClick={previous}>
+          <img src={leftArrow} />
+        </button>
+
+        {logos.map((logo) => (
+          <img
+            className={`${styles.images} ${
+              selectedIndexLogo === logo.id ? styles.active : styles.inactive
+            }`}
+            key={logo.id}
+            src={logoBrand}
+          />
+        ))}
+
+        <button className={styles.button} onClick={next}>
+          <img src={rightArrow} />
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default LogosList
